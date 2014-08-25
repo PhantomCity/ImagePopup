cc969e378a2dd8ac86a99d83a88d973bdb5620ba
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-26 00:17:28 +0400

    Update PopupImages.js

--- PopupImages.js
+++ PopupImages.js
@@ -2,10 +2,10 @@
 // @name	Show Image Popup
 // @namespace PSVScripts
 // @description	Auto change thumbnails to image direct link
-// @version	0.0.0.2
+// @version	0.0.0.3
 // @icon	http://findicons.com/icon/download/131805/image/48/png
 // @require https://greasyfork.org/scripts/4524-show-image-popup-library/code/Show%20Image%20Popup%20Library.js
 // ==/UserScript==
 
 
-alert('main script loaded !');
+//alert('main script loaded !');
