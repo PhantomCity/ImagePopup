794ecfba345993fbefda02175f70c7ceefc0e374
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-26 00:16:10 +0400

    Update PopupImagesLib.js

--- PopupImagesLib.js
+++ PopupImagesLib.js
@@ -2,8 +2,8 @@
 // @name Show Image Popup Library
 // @namespace PSVScripts
 // @description Библиотека функций PopupImage
-// @version 0.0.0.2
+// @version 0.0.0.3
 // @icon http://findicons.com/icon/download/131805/image/48/png
 // ==/UserScript==
 
-alert('Library loaded!');
+//alert('Library loaded!');
