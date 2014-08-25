80903c968ac2a903e3b08242078ecba69b6142e3
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 23:56:43 +0400

    Create PopupImagesLib.js

---
+++ PopupImagesLib.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Show Image Popup Library
+// @namespace PSVScripts
+// @description Библиотека функций PopupImage
+// @version 0.0.0.1
+// @icon http://findicons.com/icon/download/131805/image/48/png
+// ==/UserScript==
+
+alert('Library loaded');
+//
