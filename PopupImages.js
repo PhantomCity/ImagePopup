83b938aec10dc457b1cdc0fcfc5e7a4d66fb22ae
SpyDeX <SpyDeX@users.noreply.github.com>
2014-08-25 23:53:31 +0400

    Create PopupImages.js

---
+++ PopupImages.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name	Show Image Popup
+// @namespace PSVScripts
+// @description	Auto change thumbnails to image direct link
+// @version	0.0.0.1
+// @icon	http://findicons.com/icon/download/131805/image/48/png
+// ==/UserScript==
+
+
+alert('main script loaded');
