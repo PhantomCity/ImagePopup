a4166fb1d65704c8b393ca5609cfddb24d50341e
SpyDeX <SpyDeX@users.noreply.github.com>
2015-07-06 10:36:29 +0300

    Create PopupImagesHandler.js

---
+++ PopupImagesHandler.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name Show Image Popup Handlers list
+// @namespace PSVScripts
+// @description Сборщик обработчиков ссылок
+// @version 0.0.0.1
+// @require 
+// ==/UserScript==
